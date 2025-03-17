ac2949cf0d921a6834b9610302388a333ddc2e6a
Stackrats <matt@stackrats.com>
2025-03-17 11:58:12 +0800

    fix returning meta array

--- src/Services/PaginationMetaService.php
+++ src/Services/PaginationMetaService.php
@@ -6,13 +6,14 @@ namespace Stackrats\LaravelScaffoldFeature\Services;
 
 use Illuminate\Pagination\LengthAwarePaginator;
 use Stackrats\LaravelScaffoldFeature\Helpers\Pagination\BuildPaginationMeta\Actions\BuildPaginationMetaAction;
-use Stackrats\LaravelScaffoldFeature\Helpers\Pagination\BuildPaginationMeta\Data\PaginationMetaData;
 
 class PaginationMetaService
 {
     public function build(
         LengthAwarePaginator $paginator
-    ): PaginationMetaData {
-        return (new BuildPaginationMetaAction())->handle($paginator);
+    ): array {
+        return [
+            'meta' => (new BuildPaginationMetaAction())->handle($paginator),
+        ];
     }
 }
